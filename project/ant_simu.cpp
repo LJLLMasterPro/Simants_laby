72435f82f9a19af6122aa0d30c3afd522cfb8372
Xavier JUVIGNY <xavier.juvigny@onera.fr>
2018-01-23 10:01:03 +0100

    Some minor corrections

--- project/ant_simu.cpp
+++ project/ant_simu.cpp
@@ -17,7 +17,6 @@ void advance_time( const labyrinthe& land, pheronome& phen,
                    const position_t& pos_nest, const position_t& pos_food,
                    std::vector<ant>& ants, std::size_t& cpteur )
 {
-#   pragma omp parallel for
     for ( size_t i = 0; i < ants.size(); ++i )
         ants[i].advance(phen, land, pos_food, pos_nest, cpteur);
     phen.do_evaporation();
